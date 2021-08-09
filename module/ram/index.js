979ae386e425a6bccbb456abd17ab2fa1894797f
Hippo Spark (Arch) <hippospark@gmail.com>
2021-08-09 23:09:35 +0800

    为 ram 和 block_ram 模块添加了 dataUnitCount 参数。
    用于指定 ram 的容量。

--- module/ram/index.js
+++ module/ram/index.js
@@ -14,14 +14,15 @@ class RAM extends SimpleLogicModule {
     // override
     init() {
         // 数据位宽
-        this._bitWidth = this.getParameter('bitWidth');
+        this._dataBitWidth = this.getParameter('dataBitWidth');
 
         // 地址的位宽
         this._addressBitWidth = this.getParameter('addressBitWidth');
 
-        // 存储单元的数量由地址的位宽决定，比如 2 位地址共有 2 ^ 2 = 4 个存储单元，
-        // 8 位地址共有 2 ^ 8 = 256 个存储单元
-        this._storeUnitCount = Math.pow(2, this._addressBitWidth);
+        // 地址的位宽决定了最大能访问到哪个存储单元，
+        // 比如 2 位地址最大能访问第 （2 ^ 2 = 4）- 1 个（从 0 开始数）存储单元，
+        // 8 位地址最大能访问第 (2 ^ 8 = 256) - 1 个（从 0 开始数）存储单元
+        this._dataUnitCount = this.getParameter('dataUnitCount');
 
         // 输入端口
 
@@ -29,21 +30,21 @@ class RAM extends SimpleLogicModule {
         // 例如存储单元为 32 位，则地址 0 将会读第一个存储单元的值（4 个字节），
         // 地址 1 将会读取第 2 个存储单元的值（同样 4 个字节）
         this._pinAddress = this.addPin('address', this._addressBitWidth, PinDirection.input);
-        this._pinDataIn = this.addPin('dataIn', this._bitWidth, PinDirection.input);
+        this._pinDataIn = this.addPin('dataIn', this._dataBitWidth, PinDirection.input);
         this._pinWriteEnable = this.addPin('writeEnable', 1, PinDirection.input);
         this._pinReadEnable = this.addPin('readEnable', 1, PinDirection.input);
 
         this._pinClock = this.addPin('Clock', 1, PinDirection.input);
 
         // 输出端口
-        this._pinDataOut = this.addPin('dataOut', this._bitWidth, PinDirection.output);
+        this._pinDataOut = this.addPin('dataOut', this._dataBitWidth, PinDirection.output);
 
         // 存储的值
         this._clockInt32Previous = 0;
 
         // 存储单元数组
-        this._storeUnits = new Array(this._storeUnitCount);
-        for (let idx = 0; idx < this._storeUnitCount; idx++) {
+        this._storeUnits = new Array(this._dataUnitCount);
+        for (let idx = 0; idx < this._dataUnitCount; idx++) {
             this._storeUnits[idx] = 0;
         }
 
@@ -52,10 +53,10 @@ class RAM extends SimpleLogicModule {
         // https://nodejs.org/api/buffer.html
         let initialData = this.getParameter('initialData');
         if (initialData !== undefined && initialData !== null) {
-            let wordSize = this._bitWidth / 8; // 计算一个字有多少个字节，即字长
+            let wordSize = this._dataBitWidth / 8; // 计算一个字有多少个字节，即字长
             let initialDataBytes = initialData.length; //  https://nodejs.org/api/buffer.html#buffer_buf_length
             let initialDataLength = Math.ceil(initialDataBytes / wordSize); // 不足 1 个字的算 1 一个字
-            let availableLength = Math.min(this._storeUnitCount, initialDataLength);
+            let availableLength = Math.min(this._dataUnitCount, initialDataLength);
 
             for (let idx = 0; idx < availableLength; idx++) {
                 // 由 bytes 数组按字长计算得字值。
@@ -69,7 +70,7 @@ class RAM extends SimpleLogicModule {
         }
 
         // 常量信号
-        this._signalHighZ = Signal.createHighZ(this._bitWidth);
+        this._signalHighZ = Signal.createHighZ(this._dataBitWidth);
     }
 
     // override
@@ -94,7 +95,7 @@ class RAM extends SimpleLogicModule {
             if (readEnableInt32 === 1) {
                 let addressInt32 = this._pinAddress.getSignal().getLevel().toInt32();
                 let data = this._storeUnits[addressInt32];
-                let signal = Signal.createWithoutHighZ(this._bitWidth, Binary.fromInt32(data, this._bitWidth));
+                let signal = Signal.createWithoutHighZ(this._dataBitWidth, Binary.fromInt32(data, this._dataBitWidth));
                 this._pinDataOut.setSignal(signal);
             } else {
                 this._pinDataOut.setSignal(this._signalHighZ);
